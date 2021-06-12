b70885da89fc64c4a4399f925198b3818cf0ec6a
Badr Amine <amine2019badr@gmail.com>
2021-06-12 19:54:18 +0200

    Added header main screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { RootStackParamList } from '../../types'
 import News from '../screens/News.screen'
+import { View } from 'react-native'
+import Colors from '../constants/Colors';
+import Fonts from '../constants/Fonts';
 
 export const Navigation = () => {
     return <NavigationContainer>
@@ -11,9 +14,20 @@ export const Navigation = () => {
 }
 
 const Stack = createStackNavigator<RootStackParamList>()
-
+const navigatorOptions = {
+    headerTitleStyle: {
+        fontSize: 25,
+        fontFamily: Fonts.bold
+    },
+    headerTintColor: Colors.white,
+    headerShown: true,
+    headerBackground: () => <View style={{
+        flex: 1,
+        backgroundColor: Colors.cornflowerBlue
+    }} />
+}
 const RootStack = () => {
-    return <Stack.Navigator initialRouteName='News' screenOptions={{ headerShown: true }}>
+    return <Stack.Navigator initialRouteName='News' screenOptions={navigatorOptions}>
         <Stack.Screen name="News" component={News} options={{ headerTitle: 'News' }} />
     </Stack.Navigator>
 }
