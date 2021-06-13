9d8c7760ebbcd85a3f988db65127207520f57d6a
Badr Amine <amine2019badr@gmail.com>
2021-06-13 20:58:25 +0200

    Assigned interfaces

--- src/redux/types/news.types.ts
+++ src/redux/types/news.types.ts
@@ -1,34 +1,29 @@
 import moment from "moment"
 
-export const GET_TOP_STORIES = "GET_TOP_STORIES"
 export const GET_STORY_ITEM = "GET_STORY_ITEM"
 export const GET_USER = "GET_USER"
 
-export interface UserInterface {
-    id: string
-    karma:number|string
+export interface UserInterface { //NOTE determine user in the story ,rappresented as 'by: Author id'
+    id: string|number
+    author_karma_score:number|string
 }
 
-export interface StoryItemInterface {
+export interface StoriesItemInterface { //NOTE determine types of story items
     id:number|string
     title: string
     url: string
-    timestamp: moment.Moment
-    story_score: number | string
-    user:UserInterface
+    time: moment.Moment
+    score: number | string
+    by:number|string //NOTE Author as 'by: Author id' -->See JSON file on API ENDPOINT 'https://hacker-news.firebaseio.com/v0/item/${id}.json' 
 }
 
-interface GetTopStoriesAction {
-    type: typeof GET_TOP_STORIES,
-    payload: number[]
-}
 interface GetStoryItemAction {
     type: typeof GET_STORY_ITEM,
-    payload:StoryItemInterface
+    payload:StoriesItemInterface
 }
 interface GetUserAction {
     type: typeof GET_USER,
     payload:UserInterface
 }
 
-export type TopStoriesActionTypes = GetTopStoriesAction|GetStoryItemAction|GetUserAction
+export type TopStoriesActionTypes = GetStoryItemAction | GetUserAction;
