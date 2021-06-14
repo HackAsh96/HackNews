54d44d6f09f6e1086a9739e0df12a79c12d6fd18
Badr Amine <amine2019badr@gmail.com>
2021-06-14 10:15:35 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,24 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import useCachedResources from './src/hooks/useCachedResources';
+import Colors from './src/constants/Colors';
+import { CustomFonts } from './src/constants/Fonts';
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading'
 import { Navigation } from './src/navigation';
 import store from './src/redux/store';
+import { getLimitedTopStories } from './src/redux/actions';
 
 
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  if (!isLoadingComplete) {
-    return null;
-  } else {
+
+export default class App extends React.Component<{}, {}>{
+  state = {
+    isLoadingComplete: false
+  };
+  componentDidMount() {
+    this._loadResourcesAsync();
+  }
+  _loadResourcesAsync = async () => {
+    await Font.loadAsync(CustomFonts)
+  };
+  render() {
+    if (!this.state.isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={(error: any) => console.warn(error)}
+          onFinish={() => {
+            this.setState({ isLoadingComplete: true })
+            store.dispatch(getLimitedTopStories())
+          }}
+        />
+      );
+    }
     return (
       <Provider store={store}>
-        <SafeAreaProvider>
+        <SafeAreaView style={styles.safeArea}>
           <Navigation />
           <StatusBar />
-        </SafeAreaProvider>
+        </SafeAreaView>
       </Provider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: Colors.cherryPie
+  }
+})
