973737ada648fb00b0edc5484de6247c3e2c27c2
Badr Amine <amine2019badr@gmail.com>
2021-06-14 07:44:41 +0200

    Common actions for fetching

--- src/redux/actions/common.actions.ts
+++ src/redux/actions/common.actions.ts
@@ -1,8 +1,11 @@
-import { FETCH_REQUEST, FETCH_FAILURE, FetchActionTypes } from "../types"
+import { FETCH_REQUEST, FETCH_FAILURE, FetchActionTypes, FETCH_SUCCESS } from "../types"
 import { ActionCreator } from "redux";
 
 export const request: ActionCreator<FetchActionTypes> = () => {
     return { type: FETCH_REQUEST };
+}
+export const success: ActionCreator<FetchActionTypes> = () => {
+    return {type:FETCH_SUCCESS}
 }
 export const failure: ActionCreator<FetchActionTypes> = (error: any) => {
     return { type: FETCH_FAILURE, payload: error };
