105639222d1c6e66927ec00b38ce1fcba4e880e2
Badr Amine <amine2019badr@gmail.com>
2021-06-14 07:48:55 +0200

    Changed names

--- src/services/_hackNews.service.ts
+++ src/services/_hackNews.service.ts
@@ -1,16 +1,16 @@
 import axios from 'axios'
 
-const getTopStories = async () => {
+const getAllTopStories = async () => {
     try {
         return axios({
             url: 'https://hacker-news.firebaseio.com/v0/topstories.json',
             method: 'GET'
-        });
+        })
     } catch (error) {
         return console.error(error);
     }
 }
-const getStoryItem = async (id: number | string) => {
+const getStoryItemById = async (id: number | string) => {
     try {
         return axios({
             url: `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
@@ -20,7 +20,7 @@ const getStoryItem = async (id: number | string) => {
         return console.error(error);
     }
 }
-const getUser = async (id: number | string) => {
+const getUserById = async (id: number | string) => {
     try {
         return axios({
             url: `https://hacker-news.firebaseio.com/v0/user/${id}.json`,
@@ -31,4 +31,4 @@ const getUser = async (id: number | string) => {
     }
 }
 
-export {getTopStories,getStoryItem,getUser}
+export {getAllTopStories,getStoryItemById,getUserById}
